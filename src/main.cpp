9b1c436561a9ffc7a5ab2ce9f60b338a1a243b69
Lazharous <mainieri.paolo@gmail.com>
2016-08-17 19:35:27 +0200

    docs added

--- src/main.cpp
+++ src/main.cpp
@@ -42,8 +42,8 @@ void *alloc(int s)              // for some big chunks... most other allocs use 
     return b;
 };
 
-int scr_w = 800;
-int scr_h = 600;
+int scr_w = 1280;
+int scr_h = 720;
 
 void screenshot()
 {
